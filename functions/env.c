3e027d3e974ccca3cb85476f56314f7e5f5f1f09
Quayle Rolande <qrolande@ge-e5.kzn.21-school.ru>
2022-01-31 19:06:47 +0300

    success

--- functions/env.c
+++ functions/env.c
@@ -6,7 +6,7 @@
 /*   By: qrolande <qrolande@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 21:19:57 by akatlyn           #+#    #+#             */
-/*   Updated: 2022/01/30 07:47:18 by qrolande         ###   ########.fr       */
+/*   Updated: 2022/01/30 08:12:48 by qrolande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,18 @@ void	ft_env(t_shell	*shell, int fd)
 		fd = shell->fd[shell->num_pipe][1];
 	while (tmp)
 	{
-		if (tmp->key && tmp->equality && shell->full_path)
+		if (shell->full_path == NULL)
+		{
+			env_error_msg(shell);
+			return ;
+		}
+		if (tmp->key && tmp->equality)
 		{
 			ft_putstr_fd(tmp->key, fd);
 			write(fd, "=", 1);
 			if (tmp->value)
 				ft_putstr_fd(tmp->value, fd);
 			write(fd, "\n", 1);
-		}
-		else
-		{
-			env_error_msg(shell);
-			return ;
 		}
 		tmp = tmp->next;
 	}
